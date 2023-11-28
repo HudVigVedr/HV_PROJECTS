1a1a81190854bf712a650f3e0b546bbb078ecc20
ThomLems <ThomLems@TL-BLINK>
2023-11-28 17:11:48 +0100

    DH & DL upsert

--- HV_WHS/HV_DWS_EXTRACT/3_BC_wmsDH_U.py
+++ HV_WHS/HV_DWS_EXTRACT/3_BC_wmsDH_U.py
@@ -10,29 +10,24 @@ sys.path.append('C:/Python/HV_PROJECTS')
 import _AUTH
 import _DEF 
 
-script_name = "BC_wmsDH"
+script_name = "BC_wmsDH_U"
 script_cat = "DWH"
 
 # SQL Server connection settings
 connection_string = f"DRIVER=ODBC Driver 17 for SQL Server;SERVER={_AUTH.server};DATABASE={_AUTH.database};UID={_AUTH.username};PWD={_AUTH.password}"
-
 sql_table = "dbo.BC_wmsDH"
-print("SQL Server connection string created")
-
 
 # API endpoint URL (same as before) -> aanvullen
 api_url = _AUTH.end_REST_BOLTRICS_BC
 api_table = "wmsDocumentHeaders"
-api_full = api_url + "/" + api_table + "?" + "$select=announcedDate,announcedTime,arrivedDate,arrivedTime,attribute01,attribute02,attribute03,attribute04,attribute05,attribute06,attribute07,attribute08,attribute09,attribute10,billToCustomerName,billToCustomerNo,createdDateTime,createdUserID,deliveryDate,departedDate,documentDate,documentType,estimatedDepartureDate,id,modifiedDateTime,modifiedUserID,movementType,no,portFromName,portToName,postingDate,sellToCustomerName,sellToCustomerNo,shortcutDimension2Code,statusCode,vesselNo,voyageNo&company="
+api_full = api_url + "/" + api_table + "?" + "$select=announcedDate,announcedTime,arrivedDate,arrivedTime,attribute01,attribute02,attribute03,attribute04,attribute05,attribute06,attribute07,attribute08,attribute09,attribute10,billToCustomerName,billToCustomerNo,createdDateTime,createdUserID,deliveryDate,departedDate,documentDate,documentType,estimatedDepartureDate,id,modifiedDateTime,modifiedUserID,movementType,no,portFromName,portToName,postingDate,sellToCustomerName,sellToCustomerNo,shortcutDimension2Code,statusCode,vesselNo,voyageNo&$filter=systemModifiedAt gt "+ _DEF.yesterday_date +"T00:00:00Z&company="
 
-
-
-# Delete function
-def delete_sql_table(connection):
-    print("Deleting SQL table")
+def record_exists(connection, no, entity):
     cursor = connection.cursor()
-    cursor.execute(f"DELETE FROM {sql_table}")
-    connection.commit()
+    query = f"SELECT COUNT(1) FROM {sql_table} WHERE [No] = ? AND [Entity] = ?"
+    cursor.execute(query, (no, entity))
+    result = cursor.fetchone()[0] > 0
+    return result
 
 # Function to insert data into SQL Server
 def insert_data_into_sql(connection, data, sql_table, company_name):
@@ -83,16 +78,82 @@ def insert_data_into_sql(connection, data, sql_table, company_name):
         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """
 
-    for item in data:
-        values = list(item.values())
-        values.append(company_name)  # add company name to the list of values
-        cursor.execute(sql_insert, tuple(values))
+    values = [data[key]for key in data]     
+    values.append(company_name)          
+    cursor.execute(sql_insert, tuple(values))  
 
     connection.commit()
 
-   
+
+def update_record(connection, data, sql_table, doc_no, company):
+    cursor = connection.cursor()
+    sql_update = f"""
+        UPDATE {sql_table} 
+        SET [ODataEtag] = ?,
+            [Id] = ?,
+            [DocumentType] = ?,
+            [No] = ?,
+            [SellToCustomerNo] = ?,
+            [SellToCustomerName] = ?,
+            [BillToCustomerNo] = ?,
+            [BillToCustomerName] = ?,
+            [VoyageNo] = ?,
+            [MovementType] = ?,
+            [DocumentDate] = ?,
+            [PostingDate] = ?,
+            [StatusCode] = ?,
+            [CreatedDateTime] = ?,
+            [CreatedUserID] = ?,
+            [ModifiedDateTime] = ?,
+            [ModifiedUserID] = ?,
+            [AnnouncedDate] = ?,
+            [AnnouncedTime] = ?,
+            [ArrivedDate] = ?,
+            [ArrivedTime] = ?,
+            [DepartedDate] = ?,
+            [DeliveryDate] = ?,
+            [EstimatedDepartureDate] = ?,
+            [VesselNo] = ?,
+            [ShortcutDimension2Code] = ?,
+            [Attribute01] = ?,
+            [Attribute02] = ?,
+            [Attribute03] = ?,
+            [Attribute04] = ?,
+            [Attribute05] = ?,
+            [Attribute06] = ?,
+            [Attribute07] = ?,
+            [Attribute08] = ?,
+            [Attribute09] = ?,
+            [Attribute10] = ?,
+            [PortFromName] = ?,
+            [PortToName] = ?
+        WHERE [No] = ? AND [Entity] = ?
+    """
+
+    values = [data[key]for key in data]     
+    values.append(doc_no) 
+    values.append(company)          
+    cursor.execute(sql_update, tuple(values))  
+
+    connection.commit()
+
+def insert_or_update_data_into_sql(connection, data, sql_table, company_name):
+        
+    for item in data:
+ 
+        no = item['no']
+     
+        if record_exists(connection, no, company_name):
+            print(f"Updating record with No: {no} for entity: {company_name}")
+            update_record(connection, item, sql_table, no, company_name)
+            
+        else:
+            print(f"Inserting new record with No: {no} for entity: {company_name}")
+            insert_data_into_sql(connection, item, sql_table, company_name)
+
+
 if __name__ == "__main__":
-    print("Copying BC_wmsDH...")
+    print("Incremental refresh BC_wmsDH...")
     connection = pyodbc.connect(connection_string)
     threshold = 0
 
@@ -103,18 +164,16 @@ if __name__ == "__main__":
     try:
         company_names = _DEF.get_company_names(connection)
 
-        delete_sql_table(connection)
-
         for company_name in company_names:
             api = f"{api_full}{company_name}"
             api_data_generator = _DEF.make_api_request(api, _AUTH.client_id, _AUTH.client_secret, _AUTH.token_url)
-
+    
             data_to_insert = list(api_data_generator)
             row_count = len(data_to_insert)
 
             if row_count > threshold:
-                insert_data_into_sql(connection, data_to_insert, sql_table, company_name)
-                inserted_rows = _DEF.count_rows(data_to_insert)  # Assuming all rows are successfully inserted
+                insert_or_update_data_into_sql(connection, data_to_insert, sql_table, company_name)         
+                inserted_rows = _DEF.count_rows(data_to_insert)
                 total_inserted_rows += inserted_rows
 
                 if inserted_rows != row_count:
@@ -132,7 +191,6 @@ if __name__ == "__main__":
                     _AUTH.email_username,
                     _AUTH.email_password
                     )    
-
 
     except Exception as e:
         overall_status = "Error"
@@ -153,7 +211,7 @@ if __name__ == "__main__":
 
     finally:
         if overall_status == "Success":
-            success_message = f"Total rows inserted: {total_inserted_rows}."
+            success_message = f"Total rows inserted/updated: {total_inserted_rows}."
             _DEF.log_status(connection, "Success", script_cat, script_name, start_time, _DEF.datetime.now(), int((_DEF.datetime.now() - start_time).total_seconds() / 60), total_inserted_rows, success_message, "All", "N/A")
 
         elif overall_status == "Error":
